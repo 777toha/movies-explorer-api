c693fd094b3f7ba84f341638728542984d0c4d78
Anton Lyscov <anton.lyscov@yandex.ru>
2023-05-18 13:42:07 +0300

    добавление check-cookie

---
+++ src/routes/cookie.ts
@@ -0,0 +1,14 @@
+import { Router, Request, Response } from 'express';
+
+const cookieRouter = Router();
+
+cookieRouter.get('/', (req: Request, res: Response) => {
+  const cookie = req.cookies('jwt');
+  if (cookie) {
+    res.send({ valid: true });
+  } else {
+    res.send({ valid: false });
+  }
+});
+
+export default cookieRouter;
